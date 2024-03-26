aef839a2580f22ce7b9871b2205b744d9b173f14
pavan-source <47970474+Pavanramisetty@users.noreply.github.com>
2024-03-25 22:12:54 -0700

    test

---
+++ testPrograms/runner.java
@@ -0,0 +1,7 @@
+import test;
+public class runner{
+
+    public static void main(Sting[] args){
+        test.swap(10, 20)
+    }
+}
---
+++ testPrograms/testProgram.java
@@ -0,0 +1,9 @@
+public class test{
+    public swap(arg1, arg1){
+        int tmp;
+        arg1 = tmp;
+        tmp = arg2;
+        arg2 = arg1
+        return arg1, arg2;
+    }
+}
